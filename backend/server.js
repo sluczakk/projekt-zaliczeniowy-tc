2eb8d3501c68d3da52de82ff6be118700cd17778
sluczak <->
2026-03-30 19:39:00 +0200

    fix port

--- backend/server.js
+++ backend/server.js
@@ -290,6 +290,7 @@ app.get("/{*splat}", (req, res) => {
 });
 
 // START
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
